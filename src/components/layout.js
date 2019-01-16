962da8bdf01354c0a5bdd6b54d318c8dba12053f
BabboPippo <fabio@certini.net>
2019-01-16 20:50:36 +0100

    Sass e Global CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import './bootstrap.css'
-import './layout.css'
+import {createGlobalStyle} from 'styled-components';
+// import './bootstrap.css'
+// import './layout.css'
+// import '../sass/layout.scss'
 
 const Layout = ({children}) =>{
   return (
     <React.Fragment>
+      <GlobalStyle />
       {children}
     </React.Fragment>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open-sans', sans-serif;
+  color: #262626;
+  background: #fff;
+}
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -6,8 +7,14 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>Benvenuti in Gatsbyg</h3>
+    <h3>Benvenuti in Gatsby</h3>
+    <ButtonWrapper>Read me</ButtonWrapper>
   </Layout>
 )
+
+const ButtonWrapper = styled.button`
+background: blue;
+color: white;
+`
 
 export default IndexPage

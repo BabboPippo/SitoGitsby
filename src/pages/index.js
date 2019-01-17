69686a2390c6656b8ccb6c4667702c22790d1f2d
BabboPippo <fabio@certini.net>
2019-01-17 07:27:45 +0100

    React Icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -8,13 +7,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h3>Benvenuti in Gatsby</h3>
-    <ButtonWrapper>Read me</ButtonWrapper>
+    
   </Layout>
 )
 
-const ButtonWrapper = styled.button`
-background: blue;
-color: white;
-`
 
 export default IndexPage

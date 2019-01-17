4b7473554c41f197f983b9177b9c81b6fc4abcbf
BabboPippo <fabio@certini.net>
2019-01-17 07:44:07 +0100

    Aggiunta icona

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import { FaBeer } from 'react-icons/fa';
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>Benvenuti in Gatsby</h3>
-    
+    <h3>Benvenuti in Gatsby!</h3>
+    <h3> Lets go for a <FaBeer />? </h3>
   </Layout>
 )
 
